568fe965e0281a331139e40a8db97f9723529d92
mahesrao <40287230+mahesrao@users.noreply.github.com>
2024-09-08 12:52:29 +0530

    Update main.tf
    add vpc

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro" #instance type
+#Creation of VPC - vpc.tf
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = "10.0.0.0.16"
   tags = {
-      Name = "TF-Instance-1"
+    Name = "green-vpc"
   }
 }
